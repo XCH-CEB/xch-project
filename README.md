05bab75fbb60ac058d5102196d1e142101d0053f
LEXUGE <LEXUGEyky@outlook.com>
2018-08-31 14:18:48 +0800

    Merge into one repo

--- README.md
+++ README.md
@@ -1,53 +1,19 @@
-# lib_xch
-[![Build Status](https://travis-ci.org/XCH-CEB/lib-xch-ceb.svg?branch=master)](https://travis-ci.org/XCH-CEB/lib-xch-ceb)  
+# XCH Project
+![Screenshot](Screenshot.png)  
+[![Build Status](https://travis-ci.org/XCH-CEB/xch-project.svg?branch=master)](https://travis-ci.org/XCH-CEB/xch-project)  
 
-This is crate [xch-ceb](https://crates.io/crates/xch-ceb)'s official lib.  
-[lib_xch on crates.io](https://crates.io/crates/lib_xch)  
-[lib_xch's documentation](https://docs.rs/lib_xch)  
+This is XCH Project.  
+It's a chemical equation balancing toolkit written in Rust language.   
+[lib_xch](https://crates.io/crates/lib_xch)   
+
+[xch-ceb](https://crates.io/crates/xch-ceb)  
+
 
 # Goals of this project
 Swift, Small, Safe.  
 
-# Getting Started
-First of all, you should pick a version.  
-At this time (2018), add following dependency to your `Cargo.toml`:  
-```
-[dependencies]
-lib_xch = "^0.7"
-```  
-**You can use the latest version number (e.g. v0.8) to replace v0.7**
-
-# Example
-For more information, please read the source code of [xch-ceb](https://crates.io/crates/xch-ceb/)
-```rust
-extern crate lib_xch;
-
-use lib_xch::api::handler::handler_api;
-use std::io;
-
-fn main() {
-    let equation = input();
-    match handler_api::<i32>(&equation) {
-        Ok(s) => println!("{:?}", s),
-        Err((e, _)) => println!("{:?}", e),
-    };
-}
-
-// other functions
-fn input() -> String {
-    println!("[INPUT] Input the equation:");
-    let mut equation = String::new();
-    io::stdin()
-        .read_line(&mut equation)
-        .expect("[ERROR] Failed to read line!");
-    equation.pop();
-    equation
-}
-
-```
-
 # License
-Licensed under GPL-3.0
+Licensed under GPL-3.0  
 
 # Plans
 - [x] Uses regex-based parser
---
+++ xch-ceb/src/main.rs
@@ -0,0 +1,85 @@
+// Copyright 2017-2018 LEXUGE
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// extern crate(s)
+extern crate lib_xch;
+
+use lib_xch::api::handler::handler_api;
+use lib_xch::api::handler::ErrorCases;
+use lib_xch::api::structs::ChemicalEquation;
+use std::io;
+
+fn main() {
+    print_about_info();
+    let equation = input();
+    match handler_api::<i32>(&equation) {
+        Ok((c, v)) => print_ans(&c, &v),
+        Err((ErrorCases::ParserError(e), _)) => println!("{}", e),
+        Err((e, _)) => println!("{:?}", e),
+    };
+}
+
+// other functions
+fn print_about_info() {
+    println!("XCH  Copyright (C) 2017-2018  LEXUGE");
+    println!("This program comes with ABSOLUTELY NO WARRANTY;");
+    println!("This is free software, and you are welcome to redistribute it");
+    println!("under certain conditions;");
+    println!("<> by LEXUGE <LEXUGEyky@outlook.com>");
+    println!("License: GPL-3.0-only or GPL-3.0-or-later");
+}
+
+fn input() -> String {
+    println!("[INPUT] Input the equation:");
+    let mut equation = String::new();
+    io::stdin()
+        .read_line(&mut equation)
+        .expect("[ERROR] Failed to read line!");
+    equation.pop();
+    equation
+}
+
+fn print_ans(c: &ChemicalEquation, vecs: &[Vec<i32>]) {
+    println!("[OUTPUT]:");
+    for i in 0..c.sum {
+        let mut flag = false;
+        if vecs.len() == 1 {
+            print!("{}", vecs[0][i])
+        } else {
+            for (index, val) in vecs.iter().enumerate() {
+                if (val[i].is_positive()) && flag {
+                    print!("+")
+                }
+                match val[i] {
+                    0 => (),
+                    1 => {
+                        print!("{{k{}}}", index + 1);
+                        flag = true
+                    }
+                    -1 => {
+                        print!("-{{k{}}}", index + 1);
+                        flag = true
+                    }
+                    _ => {
+                        print!("{}*{{k{}}}", val[i], index + 1);
+                        flag = true
+                    }
+                }
+            }
+        }
+        print!("   ");
+    }
+    println!();
+}
